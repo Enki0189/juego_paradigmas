576464300a93c28750acb1b99034571044ff6e29
enki0189 <marudiaz0189@gmail.com>
2021-11-15 22:34:28 -0300

    se agregan modificaciones a la clase panel

--- src/tetris/Panel.java
+++ src/tetris/Panel.java
@@ -1,11 +1,47 @@
 package tetris;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel {
 
-	public Panel(int anchoVentana, int largoVentana) {
+public class Panel extends JPanel implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	private int anchoJuego;
+	private int largoJuego;
+
+	public Panel(int anchoJuego, int largoJuego) {
+		this.anchoJuego = anchoJuego;
+		this.largoJuego = largoJuego;
+	}
+
+	@Override
+	public void run() {
 		
+		
+	}
+	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension (anchoJuego,largoJuego);
+	}
+	
+	@Override
+	protected void paintComponent (Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.BLUE);
+		g.fillRect(70,70,70,70);
+		g.setColor(Color.RED);
+		g.fillRect(70,20,70,20);
+		
+	
 	}
 
 }
