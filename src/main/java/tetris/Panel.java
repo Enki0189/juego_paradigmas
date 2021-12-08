5e80057c2ee76b379557b53f7a38b111b1408fb4
Rocío Catalano <rociocatalano4@hotmail.com>
2021-12-08 19:17:21 -0300

    borro método comentado

--- src/main/java/tetris/Panel.java
+++ src/main/java/tetris/Panel.java
@@ -68,26 +68,6 @@
  		g2d.setFont(new Font("Rubik", Font.PLAIN, 50));
  		g2d.drawString(mensaje, 260, 265);
  	}
- 	
- 	/*private void infoEnPantalla(Graphics2D g2d) {
- 		//cuadrado para lineas
- 		g2d.setColor(new Color(0,0,0));
- 		g2d.fillRect(575, 330, 110, 35);
- 		g2d.setColor(new Color(222, 27, 82));
- 		g2d.drawRect(575, 330, 110, 35);
- 		g2d.setFont(new Font("Rubik", Font.PLAIN, 30));
- 		String lineas = "" + tablero.lineasBorradas;
- 		g2d.drawString(lineas, 600, 358);
- 		
- 		//cuadrado para lineas
- 		g2d.setColor(new Color(0, 0, 0));
- 		g2d.fillRect(575, 390, 110, 35);
- 		g2d.setColor(new Color(222, 27, 82));
- 		g2d.drawRect(575, 390, 110, 35);
- 		g2d.setFont(new Font("Rubik", Font.PLAIN, 30));
- 		String puntaje = "" + tablero.lineasBorradas*10;
- 		g2d.drawString(puntaje, 600, 417);
- 	}*/
 
 
  
